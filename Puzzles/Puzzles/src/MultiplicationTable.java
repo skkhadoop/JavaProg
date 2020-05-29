aae6e65027cb1318f09a19c7e1831f87d321325f
270014159 <270014159@RUS19-200788.rexelusa.net>
2020-05-29 11:41:26 -0500

    Initial Commit - SK - 20200529

---
+++ Puzzles/Puzzles/src/MultiplicationTable.java
@@ -0,0 +1,11 @@
+
+public class MultiplicationTable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+for(i=1;i<=10;i++)
+	System.out.printf("%d * %d = %d",5,i,5*i).println();
+	}
+
+}
---
+++ Puzzles/Puzzles/src/NestedMultiplicationTables.java
@@ -0,0 +1,13 @@
+
+public class NestedMultiplicationTables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+int j;
+for(j=1;j<=10;j++)
+for(i=1;i<=10;i++)
+			System.out.printf("%d * %d = %d",j,i,j*i).println();
+	}
+
+}
